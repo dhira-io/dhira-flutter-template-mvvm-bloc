670dbecddc76ba05dbf4aefdb585e92b25c69be7
sanjaybishtdhira <sanjay.bisht@dhira.ai>
2026-02-17 11:45:31 +0530

    enhance:- code refactoring done.

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  // AuthRemoteDataSource Endpoints
+  /// Endpoint for user login
+  static const String endPointLogin = '/auth/login';
+
+  /// Endpoint for user registration
+  static const String endPointRegister = '/users/add';
+}
